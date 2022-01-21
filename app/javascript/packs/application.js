f86db0e3384aab1a9440cc35e5d4dd1d1e3a645d
fumtas1k <fumta01a7@gmail.com>
2022-01-22 00:05:31 +0900

    init

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+import "bootstrap";
+import "../stylesheets/application";
+
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
