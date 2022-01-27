ab8fd7dc2fc2b3bb7fdd164178c5f7863f264b3f
fumtas1k <fumta01a7@gmail.com>
2022-01-27 15:00:47 +0900

    fix:スラッシュを除去

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'frozen-gorge-95207.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'frozen-gorge-95207.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
